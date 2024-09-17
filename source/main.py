3f6c6252d599dc7c9f64e8f0380370874c2e1b94
Hubert Stanowski <hubert.stanowski@gmail.com>
2024-09-17 17:28:12 +0100

    Add skeleton for main

---
+++ source/main.py
@@ -0,0 +1,27 @@
+from constants import *
+
+import pygame
+
+
+def main() -> None:
+    pygame.init()
+
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    pygame.display.set_caption("Snake NEAT AI")
+
+    clock = pygame.time.Clock()
+
+    while True:
+        clock.tick(FPS)
+        window.fill(BACKGROUND_COLOR)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
